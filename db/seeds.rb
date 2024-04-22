2c7647fae03134626d3369c7adab6deeae6e9ccd
James Finkelstein <jiflotr9@gmail.com>
2024-04-22 17:55:43 -0500

    act 2 finished, simple store made, yogurt themed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.destroy_all
+
+Product.create(name: "Peach Yogurt 4pk", price: 10.99, description: "Rich, creamy, Peach yogurt by Chobani")
+Product.create(name: "Strawberry Yogurt 4pk", price: 10.99, description: "Rich, creamy, Strawberry yogurt by Chobani")
+Product.create(name: "Mixed Berry Yogurt 4pk", price: 10.99, description: "Rich, creamy, Mixed Berry yogurt by Chobani")
